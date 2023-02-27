91a4972e566b57b57d4707cef138b846ad6e2418
Apoorva Aditya <apoorva9@illinois.edu>
2023-02-27 11:39:33 -0600

    Created ApiService

---
+++ lib/services/api_service.dart
@@ -0,0 +1,26 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:idk/models/failure.dart';
+
+class ApiService {
+  static const String _baseUrl = '';
+  late final Dio _dio;
+
+  ApiService() {
+    _dio = Dio(
+      BaseOptions(baseUrl: _baseUrl),
+    );
+  }
+
+  Future<Either<Failure, Unit>> postQrScanResult() async {
+    try {
+      final response = await _dio.post('', data: {});
+      if (response.statusCode != 200) {
+        return left(Failure(message: 'Posting result failed: ${response.statusMessage}'));
+      }
+      return right(unit);
+    } catch (e) {
+      return left(Failure(message: 'Exception caught: $e'));
+    }
+  }
+}
