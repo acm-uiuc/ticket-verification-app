7e721d1f6af2a0b9d832d62c1b800e0e2cd9ac89
Apoorva Aditya <apoorva9@illinois.edu>
2023-03-25 17:35:24 -0500

    FIxed API service and base URL

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,8 +4,7 @@ import 'package:ticket_verification_app/models/failure.dart';
 import 'package:ticket_verification_app/models/response_data.dart';
 
 class ApiService {
-  static const String _baseUrl =
-      'https://n6pzrhqr3je5ubaure5yarfuli0jsnpr.lambda-url.us-east-1.on.aws/';
+  static const String _baseUrl = 'https://n6pzrhqr3je5ubaure5yarfuli0jsnpr.lambda-url.us-east-1.on.aws/';
   late final Dio _dio;
 
   ApiService() {
@@ -14,19 +13,19 @@ class ApiService {
     );
   }
 
-  Future<Either<Failure, ResponseData>> getQrScanResult(
-      {required String qrCodeContent}) async {
+  Future<Either<Failure, ResponseData>> getQrScanResult({required String qrCodeContent}) async {
     try {
       final response = await _dio.get('', queryParameters: {
         'code': qrCodeContent,
       });
 
       if (response.statusCode != 200) {
-        return left(Failure(
-            message: 'Posting result failed: ${response.statusMessage}'));
+        return left(Failure(message: 'Posting result failed: ${response.statusMessage}'));
       }
-      return right(
-          ResponseData.fromJson(response.data as Map<String, dynamic>));
+      // print(response.data);
+      final result = ResponseData.fromJson(response.data as Map<String, dynamic>);
+      // print(result);
+      return right(result);
     } catch (e) {
       // rethrow;
       return left(Failure(message: 'Exception caught: $e'));
