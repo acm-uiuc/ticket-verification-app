1b5f5f6e1761813b43dd3a7e5c2da6e28159caf0
Dev Singh <dev@devksingh.com>
2023-11-07 17:07:49 -0600

    Automatic Deploy action run by github-actions

--- android/app/src/main/kotlin/com/example/idk/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ticket_verification_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/services/provider.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:ticket_verification_app/services/api_service.dart';
-
-final Provider<ApiService> apiServiceProvider = Provider<ApiService>((ref) => ApiService());
